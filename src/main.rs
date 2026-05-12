ae1cf430ae9f8768229b5f6ab6640e97a805b0f6
Eric Bitencourt de Santana <bitencourteric@hotmail.com>
2026-05-12 14:12:20 -0300

    feat(lb): small round robin with upstream calculation

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,21 @@
-use tokio::{io, net::TcpListener};
+use tokio::{
+    io::{self},
+    net::{TcpListener, TcpStream},
+};
 
 #[tokio::main]
 async fn main() -> io::Result<()> {
+    let upstreams = ["127.0.0.1:8080", "127.0.0.1:8081"];
+    let mut counter: usize = 0;
     let listener = TcpListener::bind("0.0.0.0:9999").await?;
     loop {
-        let (_tcp_stream, socket_address) = listener.accept().await?;
-        println!("connection from {}", socket_address.ip())
+        let (mut client, _socket_address) = listener.accept().await?;
+        let idx = counter % upstreams.len();
+        let upstream = upstreams[idx];
+        counter += 1;
+        tokio::spawn(async move {
+            let mut api = TcpStream::connect(upstream).await?;
+            tokio::io::copy_bidirectional(&mut client, &mut api).await
+        });
     }
 }
